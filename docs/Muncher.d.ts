177c889a638a88b5dadb74c81e9a318779dc0d32
asikpradhan <asikpradhan@users.noreply.github.com>
2023-10-17 13:52:41 +0000

    update build

---
+++ docs/Muncher.d.ts
@@ -0,0 +1,5 @@
+import React from "react";
+import { HtmlProps } from "./plugins/HtmlPlugin.tsx";
+export declare const Muncher: React.FC<MuncherProps>;
+export interface MuncherProps extends HtmlProps {
+}
---
+++ docs/plugins/HtmlPlugin.d.ts
@@ -0,0 +1,8 @@
+import React from "react";
+export declare const HtmlPlugin: React.FC<HtmlPluginProps>;
+export interface HtmlProps {
+    html?: string;
+    onChange: (html: string, characterCount: number) => void;
+}
+export interface HtmlPluginProps extends HtmlProps {
+}
